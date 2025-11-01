f6ebd8ea5d6675de3300ea2b3de8e498f6455e28
wangqihao <w-qh@foxmail.com>
2025-11-01 17:23:58 +0800

    vm.$watch

---
+++ 深入浅出 Vue.js/static/第04章：变化侦测相关的 API 实现原理/vm.$watch/dep.mjs
@@ -0,0 +1,33 @@
+let uid = 0;
+
+export default class Dep {
+  constructor() {
+    this.id = uid++;
+    this.subs = [];
+  }
+
+  addSub(sub) {
+    this.subs.push(sub);
+  }
+
+  removeSub(sub) {
+    const index = this.subs.indexOf(sub);
+    if (index > -1) {
+      return this.subs.splice(index, 1);
+    }
+  }
+
+  depend() {
+    if (globalThis.target) {
+      // this.addSub(globalThis.target);
+      globalThis.target.addDep(this);
+    }
+  }
+
+  notify() {
+    const subs = this.subs.slice();
+    for (let i = 0, l = subs.length; i < l; i++) {
+      subs[i].update();
+    }
+  }
+}
